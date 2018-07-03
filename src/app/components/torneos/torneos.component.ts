6d7ba5aade6d0f4f7b1bd8aca0b68cc9269f0b07
jlainez <jorgeld1986@gmail.com>
2018-07-03 12:56:34 +0200

    Modificada maquetación torneo

--- src/app/components/torneos/torneos.component.ts
+++ src/app/components/torneos/torneos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EquiposService} from "../equipos/equipos.service";
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-torneos',
@@ -13,6 +14,24 @@ export class TorneosComponent implements OnInit {
 
   equipoList = [];
   numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+  RONDAS = 7;
+  RATIOS = {
+    RATIO_ATQ : 250,
+    RATIO_DEF : 80,
+    RATIO_REB : 20,
+    RATIO_PAS : 20,
+    RATIO_AGR : 20,
+    RATIO_SEX : 20,
+  };
+  DIFERENCIALES = {
+    ATQ : 30,
+    DEF : 10,
+    REB : 5,
+    PAS : 5,
+    AGR : 5,
+    SEX : 5,
+  };
+
 
   torneo = {
     octavos : {
@@ -45,13 +64,14 @@ export class TorneosComponent implements OnInit {
           ronda:1,
           partidos : [
             {
-              resultado : 0,
+              marcador_equipo1 : 0,
+              marcador_equipo2 : 0,
               ganador:'',
             }
           ],
           estadoEliminatoria : {
-            victoriasEquipo1 : 0,
-            victoriasEquipo2 : 0,
+            victorias_equipo1 : 0,
+            victorias_equipo2 : 0,
           },
           ganador : null
         };
@@ -63,12 +83,189 @@ export class TorneosComponent implements OnInit {
 
   };
 
+  playGame = (eliminatoria) => {
+
+    let puntos = {
+      equipo1 : {},
+      equipo2 : {}
+    };
+
+    if(eliminatoria.ronda <= this.RONDAS){
+
+      //Generamos medias.
+      eliminatoria.equipo1.medias = this.generarMedias(eliminatoria.equipo1.jugadores);
+      eliminatoria.equipo2.medias = this.generarMedias(eliminatoria.equipo2.jugadores);
+
+      // //Creamos partido
+      // let partido ={
+      //   marcador_equipo1 : 0,
+      //   marcador_equipo2 : 0,
+      //   ganador:'',
+      // };
+      // eliminatoria.partidos.push(partido);
+
+
+      //Generamos las puntuaciones para cada atributo y equipo
+      let equipos = ['equipo1','equipo2'];
+      let atributos = ['atq','def','reb','pas','agr','sex'];
+
+      equipos.forEach((equipo)=>{
+        atributos.forEach((atributo) => {
+          puntos[equipo][atributo] = this.generarPuntuaciones(eliminatoria[equipo], atributo.toUpperCase());
+        });
+        //generamos marcador
+        eliminatoria.partidos[eliminatoria.ronda-1]['marcador_'+equipo]= Math.floor(this.generarMarcadores(equipo,puntos));
+      });
+
+
+
+      //SI GANAN LOCALES
+      if(eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo1'] > eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo2']){
+        //Marcamos ganador
+        eliminatoria.partidos[eliminatoria.ronda-1].ganador = eliminatoria.equipo1;
+        //Aumentamos victoria local en Global Eliminatoria
+        eliminatoria.estadoEliminatoria.victorias_equipo1++;
+        //Guardamos marcador del partido para que se refleje en el frontal
+        eliminatoria.ultimoPartido1 = ''+eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo1'];
+        eliminatoria.ultimoPartido2 = ''+eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo2'];
+        //Chequeamos si hay ganador de eliminatoria. En caso no de haber, aumentamos una ronda
+        if(eliminatoria.estadoEliminatoria.victorias_equipo1 !== 4 ){
+          //Al no finalizar la eliminatoria, aumentamos ronda
+          eliminatoria.ronda++;
+          //Generamos el nuevo partido
+          let partido ={
+            marcador_equipo1 : 0,
+            marcador_equipo2 : 0,
+            ganador:'',
+          };
+          eliminatoria.partidos.push(partido);
+        // Si el equipo local llega a 4 victorias lo marcamos como ganador de la eliminatoria
+        }else{
+          eliminatoria.ganador = eliminatoria.equipo1
+        }
+
+      //SI GANA VISITANTE
+      }else if(eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo1'] < eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo2']){
+        //Marcamos ganador
+        eliminatoria.partidos[eliminatoria.ronda-1].ganador = eliminatoria.equipo2;
+        //Aumentamos victoria visitante en Global Eliminatoria
+        eliminatoria.estadoEliminatoria.victorias_equipo2++;
+        //Guardamos marcador del partido para que se refleje en el frontal
+        eliminatoria.ultimoPartido1 = ''+eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo1'];
+        eliminatoria.ultimoPartido2 = ''+eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo2'];
+        //Chequeamos si hay ganador de eliminatoria. En caso no de haber, aumentamos una ronda
+        if(eliminatoria.estadoEliminatoria.victorias_equipo2 !== 4 ){
+          //Al no finalizar la eliminatoria, aumentamos ronda
+          eliminatoria.ronda++;
+          //Generamos el nuevo partido
+          let partido ={
+            marcador_equipo1 : 0,
+            marcador_equipo2 : 0,
+            ganador:'',
+          };
+          eliminatoria.partidos.push(partido);
+        // Si el equipo visitante llega a 4 victorias lo marcamos como ganador de la eliminatoria
+        }else{
+          eliminatoria.ganador = eliminatoria.equipo2;
+          //Guardamos marcador del partido para que se refleje en el frontal
+          eliminatoria.ultimoPartido1 = ''+eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo1'];
+          eliminatoria.ultimoPartido2 = ''+eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo2'];
+        }
+      //SI EMPATAN
+      }else{
+        eliminatoria.ultimoPartido1 = ''+eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo1'];
+        eliminatoria.ultimoPartido2 = ''+eliminatoria.partidos[eliminatoria.ronda-1]['marcador_equipo2'];
+        eliminatoria.partidos.slice(eliminatoria.partidos.length -1 ,1)
+      }
+
+      console.log('Eliminatoria ---> ' , eliminatoria.partidos);
+
+      // eliminatoria.ronda++
+    }
+
+  };
+
+  generarMedias = (jugadores) => {
+
+    let sumatorio = {
+      ataque : 0,
+      defensa : 0,
+      rebotes : 0,
+      pase : 0,
+      agresividad : 0,
+      sexualidad : 0
+    };
+
+    let medias = {
+      ataque : 0,
+      defensa : 0,
+      rebotes : 0,
+      pase : 0,
+      agresividad : 0,
+      sexualidad : 0
+    };
+
+    jugadores.forEach((jugador) => {
+      sumatorio.ataque += jugador.atributos.ataque;
+      sumatorio.defensa += jugador.atributos.defensa;
+      sumatorio.rebotes += jugador.atributos.rebotes;
+      sumatorio.pase += jugador.atributos.pase;
+      sumatorio.agresividad += jugador.atributos.agresividad;
+      sumatorio.sexualidad += jugador.atributos.sexualidad;
+    });
+
+    medias.ataque = sumatorio.ataque / 10;
+    medias.defensa = sumatorio.defensa / 10;
+    medias.rebotes = sumatorio.rebotes / 10;
+    medias.pase = sumatorio.pase / 10;
+    medias.agresividad = sumatorio.agresividad / 10;
+    medias.sexualidad = sumatorio.sexualidad / 10;
+
+    return medias;
+
+  };
+
+  generarRandom = (max , min) => {
+
+    let res = Math.floor(
+      Math.random() * (
+        max - min
+      ) + min
+    );
+    return res;
+  };
+
   seleccionAleatoria = () => {
 
      let num = Math.floor((Math.random() * (this.numbers.length)));
      let res = this.numbers[num];
      this.numbers.splice(num,1);
      return res;
+  };
+
+  generarPuntuaciones = (equipo,atributo) => {
+    let response;
+    switch (atributo){
+      case 'ATQ' : response = Math.floor((this.RATIOS.RATIO_ATQ * equipo.medias.ataque) /  100);break;
+      case 'DEF' : response = Math.floor((this.RATIOS.RATIO_DEF * equipo.medias.defensa) /  100);break;
+      case 'REB' : response = Math.floor((this.RATIOS.RATIO_REB * equipo.medias.rebotes) /  100);break;
+      case 'PAS' : response = Math.floor((this.RATIOS.RATIO_PAS * equipo.medias.pase) /  100);break;
+      case 'AGR' : response = Math.floor((this.RATIOS.RATIO_AGR * equipo.medias.agresividad) /  100);break;
+      case 'SEX' : response = Math.floor((this.RATIOS.RATIO_SEX * equipo.medias.sexualidad) /  100);break;
+    }
+    return response;
+  };
+
+  generarMarcadores = (equipo,puntos) => {
+    let teamOposite;
+    (equipo === 'equipo1')? teamOposite = 'equipo1': teamOposite = 'equipo2';
+
+    return  this.generarRandom(puntos[equipo].atq , puntos[equipo].atq - this.DIFERENCIALES.ATQ)
+      + this.generarRandom(puntos[equipo].pas , puntos[equipo].pas - this.DIFERENCIALES.PAS)
+      + this.generarRandom(puntos[equipo].reb , puntos[equipo].reb - this.DIFERENCIALES.REB)
+      - this.generarRandom(puntos[equipo].sex , puntos[equipo].sex - this.DIFERENCIALES.SEX)
+      - this.generarRandom(puntos[teamOposite].def , puntos[teamOposite].def - this.DIFERENCIALES.DEF)
+      - this.generarRandom(puntos[teamOposite].agr , puntos[teamOposite].agr - this.DIFERENCIALES.AGR);
   };
 
   ngOnInit() {
